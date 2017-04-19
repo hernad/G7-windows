43da39bc101278aac697fdc4f1d53acb7de2c930
hernad <hernad@bring.out.ba>
2017-04-19 17:19:41 +0200

    fix delete tasks

--- windows/delete_tasks.cmd
+++ windows/delete_tasks.cmd
@@ -1,4 +1,4 @@
-SchTasks /Delete "Hello world"
-SchTasks /Delete  "sshd greenbox 22"
-SchTasks /Delete  "sshd greenbox admin 2222"
-SchTasks /Delete "geenbox start"
+SchTasks /Delete /F /TN "Hello world"
+SchTasks /Delete /F /TN "sshd greenbox 22"
+SchTasks /Delete /F /TN "sshd greenbox admin 2222"
+SchTasks /Delete /F /TN "geenbox start"
